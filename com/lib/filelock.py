acbf4dd484c0bb50bdb14cb7a7b4604357d7f5a8
浩桔999 <haoju@judian.tech>
2021-07-13 10:10:36 +0800

    Update file lock

--- com/lib/filelock.py
+++ com/lib/filelock.py
@@ -6,15 +6,22 @@ from contextlib import contextmanager
 @contextmanager
 def file_io_lock(filename):
     """
-    通过指定文件的文件描述符执行文件控制（锁）
+    Perform the lock operation on file descriptor fd
 
     :param filename:
     :return:
     """
 
-    with open(filename, "w") as f:
-        # 排它锁
-        fcntl.flock(f.fileno(), fcntl.LOCK_EX)
-        yield
-        # 锁释放
-        fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+    def acquire(_fd):
+        """Acquire an exclusive lock"""
+        fcntl.flock(_fd, fcntl.LOCK_EX)
+
+    def release(_fd):
+        """Unlock"""
+        fcntl.flock(_fd, fcntl.LOCK_UN)
+
+    with open(filename, 'w') as f:
+        fd = f.fileno()
+        acquire(fd)
+        yield  # Perform the decorated func
+        release(fd)
