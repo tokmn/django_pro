b6b99c71be134e5d6e2e6bd7f2374ee56eba8a8a
浩桔999 <haoju@judian.tech>
2021-07-27 20:51:53 +0800

    Refactor task decorator

--- com/core_task/task.py
+++ com/core_task/task.py
@@ -7,96 +7,83 @@ from .management.task import taskapi
 from lib.exceptions import CodeError
 
 
-class TaskManager:
-    """
-    Manage the action of the task, such as: add, update, cancel
-    """
+class CronTask:
+    def __init__(self, task_func):
+        self._check_task_func(task_func)
+        self.task_func = task_func
+        self.task_name = self._gen_task_name(task_func)
+        self.task_func._cron_task = True
 
-    def __init__(self, *args, **kwargs):
-        self.task_func = None
-        self._task_name = None
+    def __call__(self, *args, **kwargs):
+        return self.task_func(*args, **kwargs)
 
     @staticmethod
     def _check_task_func(task_func):
         """
         Check task func
         """
-        if not task_func or not inspect.isfunction(task_func):
-            raise CodeError("task func required")
-
-    @property
-    def task_name(self):
-        """
-        Task name of task func
-        """
-        if not self._task_name:
-            self._task_name = self._gen_task_name(self.task_func)
-        return self._task_name
-
-    def __call__(self, task_func, *args, **kwargs):
-        self.set_task_func(task_func)
-        return self.task_func
-
-    def set_task_func(self, task_func):
-        self._check_task_func(task_func)
-        self.task_func = task_func
-        self._register_actions()
-        # Set task flag
-        self.task_func._x_task = True
-
-    def _register_actions(self):
-        """
-        Register action for task func
-        """
-        actions = [
-            self.add,
-            self.update,
-            self.cancel,
-            #
-            self.execute,
-        ]
-        for action in actions:
-            setattr(self.task_func, action.__name__, action)
+        if not inspect.isfunction(task_func):
+            raise CodeError('task func required')
 
     def _gen_task_name(self, task_func):
+        """
+        Generate task name
+        """
         module_name = task_func.__module__
-        if module_name == "__main__":
-            main_file = getattr(sys.modules[module_name], "__file__")
+        if module_name == '__main__':
+            main_file = getattr(sys.modules[module_name], '__file__')
             module_name = os.path.splitext(os.path.basename(main_file))[0]
-        return "{}:{}".format(module_name, task_func.__name__)
+        return '{}:{}'.format(module_name, task_func.__name__)
 
-    def execute(self, *args, **kwargs):
-        """
-        Execute task func
-        """
-        return self.task_func(*args, **kwargs)
-
-    def add(
+    def cron_task(
             self,
-            task_attr: str,
-            *args,
+            task_attr,
             run_at: datetime.datetime = None,
             extra: dict = None,
-            remark: str = None,
-            **kwargs
+            remark: str = None
     ):
-        taskapi.add_task(task_name=self.task_name, task_attr=task_attr, run_at=run_at,
-                         task_args=args, task_kwargs=kwargs, extra=extra, remark=remark)
-
-    def update(
-            self,
-            task_attr: str,
-            *args,
-            run_at: datetime.datetime = None,
-            extra: dict = None,
-            remark: str = None,
-            **kwargs
-    ):
-        taskapi.update_task(task_name=self.task_name, task_attr=task_attr, run_at=run_at,
-                            task_args=args, task_kwargs=kwargs, extra=extra, remark=remark)
-
-    def cancel(self, task_attr):
-        taskapi.cancel_task(self.task_name, task_attr)
+        return _TaskController(task_name=self.task_name,
+                               task_attr=task_attr, run_at=run_at, extra=extra, remark=remark)
 
 
-task_manager = TaskManager
+class _TaskController:
+    def __init__(self, task_name, task_attr, run_at, extra, remark):
+        self.task_name = task_name
+        self.task_attr = task_attr
+        self.run_at = run_at
+        self.extra = extra
+        self.remark = remark
+        #
+        self.task_args = None
+        self.task_kwargs = None
+
+    def params(self, *args, **kwargs):
+        self.task_args = args
+        self.task_kwargs = kwargs
+        return self
+
+    def add(self):
+        taskapi.add_task(task_name=self.task_name,
+                         task_attr=self.task_attr,
+                         run_at=self.run_at,
+                         task_args=self.task_args,
+                         task_kwargs=self.task_kwargs,
+                         extra=self.extra,
+                         remark=self.remark
+                         )
+
+    def update(self):
+        taskapi.update_task(task_name=self.task_name,
+                            task_attr=self.task_attr,
+                            run_at=self.run_at,
+                            task_args=self.task_args,
+                            task_kwargs=self.task_kwargs,
+                            extra=self.extra,
+                            remark=self.remark
+                            )
+
+    def cancel(self):
+        taskapi.cancel_task(self.task_name, self.task_attr)
+
+
+cron_task = CronTask
